e76af4fc82b9ef0ba5267632dbe7621480f79716
llooo7 <l.lorenci@student.tudelft.nl>
2023-11-13 16:43:57 +0100

    Update aeroforces.py

--- aeroforces.py
+++ aeroforces.py
@@ -1,22 +1,32 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy as sp
+from scipy import interpolate
 
 data = open("MainWing_a=0.00_v=10.00ms.txt")
 ypos = []
 force = []
-ints = []
 count = 0
+M = 0.77
+a = 343 
+v = M*a
+rho = 1.225
 
 for line in data.readlines():
     count += 1
     items = line.split(' ')
     #print(items)
-    ints.append(count)
-    ypos.append(float(items[0]))
-    force.append(float(items[3]))
+    if float(items[0]) > 0:
+        ypos.append(float(items[0]))
+        force.append(float(items[3]))
 
-print(ypos)
+data.close()
+f = sp.interpolate.interp1d(ypos,force,kind='quadratic',fill_value="extrapolate")
 
-plt.plot(ints,force)
-plt.show()
+#def dynpress(c_l,vel):
+#interpolates as linear between known datapoints
+def force(x): 
+    return f(abs(x))
+
+print(force(12))
