f33850d411d8aaa0e39ba06ed58e84d577696586
llooo7 <l.lorenci@student.tudelft.nl>
2023-11-15 15:10:57 +0100

    aero force distribution working

--- aeroforces.py
+++ aeroforces.py
@@ -3,30 +3,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 from scipy import interpolate
+import isa
+import wing
 
-data = open("MainWing_a=0.00_v=10.00ms.txt")
-ypos = []
-force = []
-count = 0
-M = 0.77
-a = 343 
-v = M*a
-rho = 1.225
+data = [open("MainWing_a=0.00_v=10.00ms.txt"), open("MainWing_a=10.00_v=10.00ms.txt")]
+aoas = [0,10]
 
-for line in data.readlines():
-    count += 1
-    items = line.split(' ')
-    #print(items)
-    if float(items[0]) > 0:
-        ypos.append(float(items[0]))
-        force.append(float(items[3]))
+ypos = [[],[]]
+force = [[],[]]
 
-data.close()
-f = sp.interpolate.interp1d(ypos,force,kind='quadratic',fill_value="extrapolate")
+i = 0
+for datapoint in data:
+    ypos.append([])
+    force.append([])
+    for line in datapoint.readlines():
+        items = line.split(' ')
+        if float(items[0]) > 0:
+            ypos[i].append(float(items[0]))
+            force[i].append(float(items[3]))
+    datapoint.close()
+    i += 1
 
-#def dynpress(c_l,vel):
-#interpolates as linear between known datapoints
-def force(x): 
-    return f(abs(x))
+f0 = sp.interpolate.interp1d(ypos[0],force[0],kind='quadratic',fill_value="extrapolate")
+f10 = sp.interpolate.interp1d(ypos[1],force[1],kind='quadratic',fill_value="extrapolate")
 
-print(force(12))
+def q(h,v,S,CL):
+    return 0.5 * float(isa.getDensity(h)) * v * v * S * CL
+
+def aeroForce(x,v = 10,a = 0,h = 0): #x = point on the wing, v = velocity, a = angle of attack, given PER UNIT SPAN 
+    return q(h,v,wing.chord(x),f0(x)) + 1/10 * q(h,v,wing.chord(x),f10(x))
+
+###Graph for debugging purposes
+xVal = []
+yVal = []
+for i in range(int(10 * wing.span /2)):
+    xVal.append(i/10)
+    yVal.append(aeroForce(i/10))
+plt.plot(xVal,yVal)
+plt.show()
