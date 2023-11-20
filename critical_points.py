4b9652c053997cc1f5dfd555b51b7293495271e8
AparteSfear <150688853+AparteSfear@users.noreply.github.com>
2023-11-20 17:01:48 +0100

    critical points

--- critical_points.py
+++ critical_points.py
@@ -7,31 +7,28 @@ from aeroforces import *
 from isa import *
 
 
-n = [1]
-mass = [2]
-v = [3] 
-h = [4]
+n = [1,2,3,4]
+mass = [2,3,4,5]
+v = [3,4,5,6] 
+h = [4,5,6,7]
 
 
 for n,mass,v,h in zip(n,mass,v,h):
     
-    Weight = mass *9.81
+    Weight = mass*9.81
     v = v*np.sqrt(1.225/h)
     q = 1/2*getDensity(h)*v**2
     CLd = n*Weight/(0.5*h*v**2*surface)
     CL0 = 0.108651
     CL10 = 0.372901
     factor = (CLd-CL0)/(CL10-CL0)
-    alpha = np.arcsin(factor*np.sin(10/57.3))
+    alpha = np.arcsin(factor*np.sin(10/57.3))*57.3
     
     def lift_dist():
-        return curveFit(v,alpha,h,normalAeroForce)
+        a,b,c,d = curveFit(v,alpha,h,normalAeroForce)
+        return 
 
     
-    
- 
-print(lift_dist())
-
     
     
     
