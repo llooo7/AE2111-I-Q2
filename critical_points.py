ead12fd2edd4ab2c08adb5725dd50fb9070cf52c
AparteSfear <150688853+AparteSfear@users.noreply.github.com>
2023-11-20 17:34:04 +0100

    critical points

--- critical_points.py
+++ critical_points.py
@@ -5,6 +5,7 @@ import scipy as sp
 from wing import *
 from aeroforces import *
 from isa import *
+from bending_moment import *
 
 
 n = [1,2,3,4]
@@ -19,15 +20,14 @@ for n,mass,v,h in zip(n,mass,v,h):
     v = v*np.sqrt(1.225/h)
     q = 1/2*getDensity(h)*v**2
     CLd = n*Weight/(0.5*h*v**2*surface)
-    CL0 = 0.108651
-    CL10 = 0.372901
+    CL0 = 0.383907
+    CL10 = 1.2045817
     factor = (CLd-CL0)/(CL10-CL0)
     alpha = np.arcsin(factor*np.sin(10/57.3))*57.3
     
-    def lift_dist():
-        a,b,c,d = curveFit(v,alpha,h,normalAeroForce)
-        return 
+    a,b,c,d = plot(n,mass,v,h,alpha)
 
+print(a)
     
     
     
