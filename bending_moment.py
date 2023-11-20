60878302a7e509a036ded745bf8764b2c4dbd88d
juanbretondelacierva <150688836+juanbretondelacierva@users.noreply.github.com>
2023-11-20 17:06:23 +0100

    Update bending_moment.py

--- bending_moment.py
+++ bending_moment.py
@@ -10,7 +10,7 @@ import aeroforces
 
 data = [open("MainWing_a=0.00_v=10.00ms.txt"), open("MainWing_a=10.00_v=10.00ms.txt")]
 aoas = [0,10]
-ypos0, lcoe0, ypos1, lcoe1 = np.array([]), np.array([]), np.array([]), np.array([])
+ypos0, lcoe0, ypos1, lcoe1, dcoe0, dcoe1 = np.array([]), np.array([]), np.array([]), np.array([]), np.array([]), np.array([])
 
 # Aircraft parameters
 def chord(x):
@@ -25,11 +25,6 @@ Lambda_c2 = 0.453399865
 thrust = 96500
 dz = 0.05*chord(engine_ypos)+1.35/2
 
-n = 1
-mass = 1
-v = 100
-h = 1
-
 # Read files
 i = 0
 for datapoint in data:
@@ -40,14 +35,13 @@ for datapoint in data:
             if i == 0:
                 ypos0 = np.append(ypos0, float(items[0]))
                 lcoe0 = np.append(lcoe0, float(items[3]))
-                #dcoe[i].append(float(items[3]))
+                dcoe0 = np.append(dcoe0, float(items[3]))
             elif i == 1:
                 ypos1 = np.append(ypos1, float(items[0]))
                 lcoe1 = np.append(lcoe1, float(items[3]))
-                #dcoe[i].append(float(items[3]))
+                dcoe1 = np.append(dcoe1, float(items[3]))
     datapoint.close()
     i += 1
-
 # Load functions
 
 def wing_load_dist(x):
@@ -60,18 +54,27 @@ def torque_dist(x):
 def t(x):
     return x
 
-def integrate_spline(ypos, lcoe, n=1, mass=320000, v=10, h=1):
+def normal_force(x, a):
+    l_0 = interpolate.InterpolatedUnivariateSpline(ypos0,lcoe0,k=5)
+    l_10 = interpolate.InterpolatedUnivariateSpline(ypos1,lcoe1,k=5)
+    d_0 = interpolate.InterpolatedUnivariateSpline(ypos0,dcoe0,k=5)
+    d_10 = interpolate.InterpolatedUnivariateSpline(ypos1,dcoe1,k=5)
+    #return ((l_0(x) + l_10(x))/10*a + l_0(x)) / np.cos(a / 57.2958)
+    return np.sqrt(pow((l_0(x) + a * 1/10 * l_10(x)) / np.cos(a / 57.2958),2) + pow((d_0(x) + a * 1/10 * d_10(x)) / np.cos(a / 57.2958),2))
+
+def integrate_spline(ypos, lcoe, n=1, mass=320000, v=10, h=1, a=0):
     global x_load___, y_load___, x_load___other, y_load___other, x_shear___, y_shear___, x_moment___, y_moment___, x_torque___, y_torque___
-    l0 = interpolate.InterpolatedUnivariateSpline(ypos,lcoe,k=5)
     x_load___, y_load___, x_load___other, y_load___other  = np.array([]), np.array([]), np.array([]), np.array([])
     
     
     for i in range(0, int(L*1000)):
         if i == int(engine_ypos*1000):
-            y_load___ = np.append(y_load___, float(isa.getDensity(h))*l0(i/1000)/2*v*v*chord(i/1000) + n*(- engine_weight - wing_load_dist(i/1000)))
+            y_load___ = np.append(y_load___, float(isa.getDensity(h))*normal_force(i/1000, a)/2*v*v*chord(i/1000) + n*(- engine_weight - wing_load_dist(i/1000)))
+            #y_load___ = np.append(y_load___, aeroforces.normalAeroForce(i/1000, v, a, h) + n*(- engine_weight - wing_load_dist(i/1000)))
             y_load___other = np.append(y_load___other, - engine_weight - wing_load_dist(i/1000))
         else:
-            y_load___ = np.append(y_load___, float(isa.getDensity(h))*l0(i/1000)/2*v*v*chord(i/1000) + n*(- wing_load_dist(i/1000)))
+            y_load___ = np.append(y_load___, float(isa.getDensity(h))*normal_force(i/1000, a)/2*v*v*chord(i/1000) + n*(- wing_load_dist(i/1000)))
+            #y_load___ = np.append(y_load___, aeroforces.normalAeroForce(i/1000, v, a, h) + n*(- engine_weight - wing_load_dist(i/1000)))
             y_load___other = np.append(y_load___other, - wing_load_dist(i/1000))
         x_load___ = np.append(x_load___, i/1000)
 
@@ -111,23 +114,44 @@ def integrate_spline(ypos, lcoe, n=1, mass=320000, v=10, h=1):
             y_torque___ = np.append(y_torque___, torque(i/1000) - torque(L))
 
 
-def plot(n, mass, v, h):
-    fig, ax = plt.subplots(2, 4, constrained_layout=True)
-    integrate_spline(ypos0, lcoe0, n, mass, v, h)
+def plot(n, mass, v, h, a):
+    fig, ax = plt.subplots(3, 4, constrained_layout=True)
+    integrate_spline(ypos0, lcoe0, n, mass, v, h, 0)
     #ax[0][0].plot(ypos0, lcoe0, "o")
     #ax[0][0].plot(x_load___other, y_load___other, color="red", linestyle="--")
+    ax[0][0].set_ylabel("Angle of attack 0º")
+    ax[0][0].set_title("Load distribution diagrams")
     ax[0][0].plot(x_load___, y_load___, label="Interpolated Spline 1")
+    ax[0][1].set_title("Shear diagrams")
     ax[0][1].plot(x_shear___, y_shear___, label="Shear stress 1")
+    ax[0][2].set_title("Bending moment diagrams")
     ax[0][2].plot(x_moment___, y_moment___, label="Bending moment 1")
+    ax[0][3].set_title("Torque diagrams")
     ax[0][3].plot(x_torque___, y_torque___, label="Torque 1")
-    integrate_spline(ypos1, lcoe1, n, mass, v, h)
+    integrate_spline(ypos1, lcoe1, n, mass, v, h, 10)
     #ax[1][0].plot(ypos1, lcoe1, "o")
     #ax[1][0].plot(x_load___other, y_load___other, color="red", linestyle="--")
+    ax[1][0].set_ylabel("Angle of attack 10º")
     ax[1][0].plot(x_load___, y_load___, label="Interpolated Spline 2")
     ax[1][1].plot(x_shear___, y_shear___, label="Shear stress 2")
     ax[1][2].plot(x_moment___, y_moment___, label="Bending moment 2")
     ax[1][3].plot(x_torque___, y_torque___, label="Torque 2")
+    integrate_spline(ypos1, lcoe1, n, mass, v, h, a)
+    #ax[1][0].plot(ypos1, lcoe1, "o")
+    #ax[1][0].plot(x_load___other, y_load___other, color="red", linestyle="--")
+    ax[2][0].set_ylabel("Angle of attack "+str(a)+"º")
+    ax[2][0].plot(x_load___, y_load___, label="Interpolated Spline 2")
+    ax[2][1].plot(x_shear___, y_shear___, label="Shear stress 2")
+    ax[2][2].plot(x_moment___, y_moment___, label="Bending moment 2")
+    ax[2][3].plot(x_torque___, y_torque___, label="Torque 2")
     plt.show()
 
 # MAIN    
-plot(n, mass, v, h)
+
+n = 1
+mass = 1
+v = 50
+h = 1
+a = 5
+
+plot(n, mass, v, h, a)
