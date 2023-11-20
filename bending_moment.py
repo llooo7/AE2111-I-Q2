057b3fd990f4c0b4ee16d9ff6f84f7dd160df1da
AparteSfear <150688853+AparteSfear@users.noreply.github.com>
2023-11-20 21:44:07 +0100

    finished critical points for shear

--- bending_moment.py
+++ bending_moment.py
@@ -116,7 +116,7 @@ def integrate_spline(n=1, mass=320000, v=10, h=1, a=0):
 
 
 def plot(n, mass, v, h, a):
-    fig, ax = plt.subplots(1, 4, constrained_layout=True)
+    #fig, ax = plt.subplots(1, 4, constrained_layout=True)
     """
     integrate_spline(ypos0, lcoe0, n, mass, v, h, 0)
     #ax[0][0].plot(ypos0, lcoe0, "o")
@@ -142,15 +142,15 @@ def plot(n, mass, v, h, a):
     integrate_spline(n, mass, v, h, a)
     #ax[1][0].plot(ypos1, lcoe1, "o")
     #ax[1][0].plot(x_load___other, y_load___other, color="red", linestyle="--")
-    ax[0].set_title("Load distribution diagram")
-    ax[0].plot(x_load___, y_load___, label="Interpolated Spline 2")
-    ax[1].set_title("Shear stress diagram")
-    ax[1].plot(x_shear___, y_shear___, label="Shear stress 2")
-    ax[2].set_title("Bending moment diagram")
-    ax[2].plot(x_moment___, y_moment___, label="Bending moment 2")
-    ax[3].set_title("Torque diagram")
-    ax[3].plot(x_torque___, y_torque___, label="Torque 2")
-    plt.show()
+    #ax[0].set_title("Load distribution diagram")
+    #ax[0].plot(x_load___, y_load___, label="Interpolated Spline 2")
+    #ax[1].set_title("Shear stress diagram")
+    #ax[1].plot(x_shear___, y_shear___, label="Shear stress 2")
+    #ax[2].set_title("Bending moment diagram")
+    #ax[2].plot(x_moment___, y_moment___, label="Bending moment 2")
+    #ax[3].set_title("Torque diagram")
+    #ax[3].plot(x_torque___, y_torque___, label="Torque 2")
+    #plt.show()
     return y_load___, y_shear___, y_moment___, y_torque___
 
 # MAIN    
--- critical_points.py
+++ critical_points.py
@@ -7,15 +7,23 @@ from aeroforces import *
 from isa import *
 from bending_moment import *
 
+fig, ax = plt.subplots()
+span = np.arange(0,12.815,0.001)
 
-n = [1,2,3,4]
-mass = [2,3,4,5]
-v = [3,4,5,6] 
-h = [4,5,6,7]
+n_values = [1.21,-2.23,3.23,-1.1]
+mass_values = [2,3,4,5]
+v_values = [3,4,5,6] 
+h_values = [4,5,6,7]
+shear = []
+moment = []
+torsion = []
+maximum = []
+checkmax = []
+checkmin = []
 
 
-for n,mass,v,h in zip(n,mass,v,h):
-    
+for n,mass,v,h in zip(n_values,mass_values,v_values,h_values):
+
     Weight = mass*9.81
     v = v*np.sqrt(1.225/h)
     q = 1/2*getDensity(h)*v**2
@@ -26,9 +34,28 @@ for n,mass,v,h in zip(n,mass,v,h):
     alpha = np.arcsin(factor*np.sin(10/57.3))*57.3
     
     a,b,c,d = plot(n,mass,v,h,alpha)
+    shear.append(b)
+    maximum.append(b[0])
+    
+for i in shear:  
+    
+    plt.plot(span, i, color = 'red')
+    
+plt.axhline(y=0, color='black', linestyle='-', linewidth=0.8)  
+plt.axvline(x=0, color='black', linestyle='-', linewidth=0.8)
+plt.xlim(0,12.815)
+plt.xlabel('y')
+plt.ylabel('I')
+plt.grid(True)
 
-print(a)
+plt.show()
+
+
+
+idx1 = maximum.index(max(maximum))
+idx2 = maximum.index(min(maximum))
+
+print(n_values[idx1])
     
-    
-    
+ 
     
