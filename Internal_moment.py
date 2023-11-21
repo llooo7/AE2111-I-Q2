1399cc310c1c28371a5659ea4fcfd7bcea05daf1
AparteSfear <150688853+AparteSfear@users.noreply.github.com>
2023-11-21 14:01:36 +0100

    Added moment and torsion

---
+++ Internal_moment.py
@@ -0,0 +1,27 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+from critical_points import *
+
+for i in moment:
+    plt.plot(span, i, color = 'blue')
+    
+plt.axhline(y=0, color='black', linestyle='-', linewidth=0.8)  
+plt.axvline(x=0, color='black', linestyle='-', linewidth=0.8)
+plt.xlim(0,12.815)
+plt.xlabel('y')
+plt.ylabel('I')
+plt.grid(True)
+
+plt.show()
+
+
+
+idx1 = maximum2.index(max(maximum2))
+idx2 = maximum2.index(min(maximum2))
+
+n_crit_max = n_values[idx1]
+n_crit_min = n_values[idx2]
+
+print(n_crit_max,n_crit_min)
