046fddb97705aa7bc865488fed5afc9d85ce72d8
juanbretondelacierva <150688836+juanbretondelacierva@users.noreply.github.com>
2023-11-22 13:59:32 +0100

    Update bending_moment.py

--- bending_moment.py
+++ bending_moment.py
@@ -74,7 +74,7 @@ def integrate_spline(n=1, mass=320000, v=10, h=1, a=0):
             y_load___other = np.append(y_load___other, - engine_weight - wing_load_dist(i/1000))
         else:
             y_load___ = np.append(y_load___, float(isa.getDensity(h))*normal_force(i/1000, a)/2*v*v*chord(i/1000) + n*(- wing_load_dist(i/1000)))
-            #y_load___ = np.append(y_load___, aeroforces.normalAeroForce(i/1000, v, a, h) + n*(- engine_weight - wing_load_dist(i/1000)))
+            #y_load___ = np.append(y_load___, aeroforces.normalAeroForce(i/1000, v, a, h) + n*(- wing_load_dist(i/1000)))
             y_load___other = np.append(y_load___other, - wing_load_dist(i/1000))
         x_load___ = np.append(x_load___, i/1000)
 
@@ -100,8 +100,8 @@ def integrate_spline(n=1, mass=320000, v=10, h=1, a=0):
     x_Ndy___, y_Ndy___ = np.array([]), np.array([])
     for i in range(0, int(L*1000)):
         x_Ndy___ = np.append(x_Ndy___, i/1000)
-        #y_Ndy___ = np.append(y_Ndy___, load(i/1000)*torque_dist(i/1000) + t(i/1000))
-        y_Ndy___ = np.append(y_Ndy___, aeroforces.c4moment(i/1000, 0, h)*v*v/2*chord(i/1000))
+        y_Ndy___ = np.append(y_Ndy___, load(i/1000)*torque_dist(i/1000) +aeroforces.c4moment(i/1000, v, a, h))
+        #y_Ndy___ = np.append(y_Ndy___, float(isa.getDensity(h))*aeroforces.c4moment(i/1000, 0, h)*v*v/2*chord(i/1000))
 
     Ndy = interpolate.InterpolatedUnivariateSpline(x_Ndy___,y_Ndy___,k=5)
     torque = Ndy.antiderivative(1)
@@ -116,7 +116,7 @@ def integrate_spline(n=1, mass=320000, v=10, h=1, a=0):
 
 
 def plot(n, mass, v, h, a):
-    #fig, ax = plt.subplots(1, 4, constrained_layout=True)
+    fig, ax = plt.subplots(1, 4, constrained_layout=True)
     """
     integrate_spline(ypos0, lcoe0, n, mass, v, h, 0)
     #ax[0][0].plot(ypos0, lcoe0, "o")
@@ -142,16 +142,16 @@ def plot(n, mass, v, h, a):
     integrate_spline(n, mass, v, h, a)
     #ax[1][0].plot(ypos1, lcoe1, "o")
     #ax[1][0].plot(x_load___other, y_load___other, color="red", linestyle="--")
-    #ax[0].set_title("Load distribution diagram")
-    #ax[0].plot(x_load___, y_load___, label="Interpolated Spline 2")
-    #ax[1].set_title("Shear stress diagram")
-    #ax[1].plot(x_shear___, y_shear___, label="Shear stress 2")
-    #ax[2].set_title("Bending moment diagram")
-    #ax[2].plot(x_moment___, y_moment___, label="Bending moment 2")
-    #ax[3].set_title("Torque diagram")
-    #ax[3].plot(x_torque___, y_torque___, label="Torque 2")
-    #plt.show()
-    return y_load___, y_shear___, y_moment___, y_torque___
+    ax[0].set_title("Load distribution diagram")
+    ax[0].plot(x_load___, y_load___, label="Interpolated Spline 2")
+    ax[1].set_title("Shear stress diagram")
+    ax[1].plot(x_shear___, y_shear___, label="Shear stress 2")
+    ax[2].set_title("Bending moment diagram")
+    ax[2].plot(x_moment___, y_moment___, label="Bending moment 2")
+    ax[3].set_title("Torque diagram")
+    ax[3].plot(x_torque___, y_torque___, label="Torque 2")
+    plt.show()
+    return y_load___, y_shear___, y_moment___, y_torque___, x_load___
 
 # MAIN    
 
