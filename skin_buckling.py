01523b9680e44be11ad92314f2b9fe572619cb53
juanbretondelacierva <150688836+juanbretondelacierva@users.noreply.github.com>
2023-12-06 15:17:54 +0100

    cool things

--- skin_buckling.py
+++ skin_buckling.py
@@ -2,15 +2,27 @@ from math import pi
 from matplotlib import pyplot as plt
 import numpy as np
 from Internal_moment import momentmin
+from Moment_of_inertia import moment_of_inertia, area, chord_length
 
 poisson = 0.3
 kc = 5
 E = 69*(10**9)
 t_b = 0.002
+A_str = 1
+n_str_top = 1
+n_str_bottom =  1
+t_spar = 1
+t_skin = 1
+t_str = 1
+h_str = 1
 
 M_x = momentmin
-I_xx = 2
+I_xx = moment_of_inertia(A_str,n_str_top,n_str_bottom,t_spar,t_skin,t_str,h_str)
 y = 1 #insert height of the skin panel
+
+def t_b(y):
+    return (t_skin/(area(chord_length(y), 1, 1)[4]))
+
 
 def skin_buckling_crit(poisson, kc, E, t_b):
     return (((pi**2*kc*E)/(12*(1-poisson**2)))*(t_b**2))
@@ -24,7 +36,7 @@ def safety_margin(a, b):
 
 def plot_skin_buckling(poisson, kc, E, t_b, M_x, I_xx, y):
     fig, ax = plt.subplots()
-    ytab = [safety_margin(skin_buckling_crit(poisson, kc, E, t_b), skin_stress(M_x(x), I_xx, y)) for x in range(1, 12815)]
+    ytab = [safety_margin(skin_buckling_crit(poisson, kc, E, t_b(x)), skin_stress(M_x(x), I_xx(x), y)) for x in range(1, 12815)]
     xtab = np.array([])
     for i in range (1, 12815):
         xtab = np.append(xtab, i/1000)
