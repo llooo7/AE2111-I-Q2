067e570b9c57c8fea8ae3ce2b84968b2a501f3ed
juanbretondelacierva <150688836+juanbretondelacierva@users.noreply.github.com>
2023-12-11 16:51:23 +0100

    Update rib_placing.py

--- rib_placing.py
+++ rib_placing.py
@@ -3,7 +3,7 @@ from Moment_of_inertia import area, chord_length
 from matplotlib import pyplot as plt
 
 aspect_ratio = 0.5
-rib_placing = [0.001]
+rib_placing = [0]
 
 def plot_wingspars():
     xtab = np.arange(0, 12.815, 0.001)
@@ -14,7 +14,7 @@ def find_rib_placing(ytab):
     for i in np.arange(0, 12.815, 0.001):
         # Ensure that we have enough distance between ribs
         if i - rib_placing[-1] >= aspect_ratio * float(ytab[int(i*1000)]) * 2:
-            rib_placing.append(i)
+            rib_placing.append(round(i, 3))
 
 ytab = plot_wingspars()
 find_rib_placing(ytab)
